90c6958739badf47aa3c245ab7b09d58db77c77f
Vadim-vep <vadim.vep@gmail.com>
2023-11-27 17:08:56 +0100

    commit v1.0.4

--- section_12/main.go
+++ section_12/main.go
@@ -1,11 +1,5 @@
-package main
-
-import (
-	"fmt"
-	"math/rand"
-)
-
 /*
+package main
 func main() {
 	x := rand.Intn(251)
 	fmt.Printf("the value is %v and it's name is x \n", x)
@@ -19,7 +13,9 @@ func main() {
 
 }
 */
+
 /*
+package main
 func init() {
 	fmt.Println("This is where the initialization of my my program starts")
 }
@@ -41,6 +37,7 @@ func main() {
 */
 
 /*
+package main
 func main() {
 	x := rand.Intn(10)
 	y := rand.Intn(10)
@@ -65,7 +62,7 @@ func main() {
 
 }
 */
-
+/*
 func main() {
 	x := rand.Intn(10)
 	y := rand.Intn(10)
@@ -82,5 +79,109 @@ func main() {
 		fmt.Printf("y is not equal to 5")
 	default:
 		fmt.Printf("None of the conditions are met")
+	}
+}
+*/
+
+/*
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	for i := 0; i < 100; i++ {
+		fmt.Printf("i is %v \n", i)
+		x := rand.Intn(10)
+		y := rand.Intn(10)
+		fmt.Printf("The variables are x=%v, y=%v\n", x, y)
+
+		switch {
+		case x < 4 && y < 4:
+			fmt.Printf("Both x and y are less than 4 \n")
+		case x > 6 && y > 6:
+			fmt.Printf("Both x and y are greater than 6 \n")
+		case x >= 4 && x <= 6:
+			fmt.Printf("x is equal or greater than 4 and less or equal to 6 \n")
+		case y != 5:
+			fmt.Printf("y is not equal to 5 \n")
+		default:
+			fmt.Printf("None of the conditions are met \n")
+		}
+	}
+}
+*/
+
+/*
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	for i := 0; i < 42; i++ {
+		x := rand.Intn(5)
+		switch {
+		case x == 0:
+			fmt.Printf("x is equal to %v \n", x)
+		case x == 1:
+			fmt.Printf("x is equal to %v \n", x)
+		case x == 2:
+			fmt.Printf("x is equal to %v \n", x)
+		case x == 3:
+			fmt.Printf("x is equal to %v \n", x)
+		default:
+			fmt.Printf("x is equal to %v \n", x)
+		}
+	}
+}
+*/
+
+/*
+package main
+
+import "fmt"
+
+func main() {
+	i := 20
+	for i >= 0 {
+		fmt.Println(i)
+		i--
+	}
+}
+*/
+
+/*
+package main
+
+import "fmt"
+
+func main() {
+	i := 20
+	for {
+		fmt.Println(i)
+		i--
+		if i < 0 {
+			break
+		}
+	}
+}
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	for z := 0; z < 20; z++ {
+		if z%2 == 0 {
+			continue //skips the iteration of the for cycle if z is EVEN
+		}
+		fmt.Println(z)
 	}
 }
